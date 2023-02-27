3021b8baf34c1feca84004d11240047a26763cc5
ariunaamy <ariun.myagmar@gmail.com>
2023-02-27 15:36:15 -0500

    complete

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -2,9 +2,11 @@ import Highlights from "./Highlights";
 import NewsletterForm from "./NewsletterForm";
 import splashImage from "./andrew-s-unsplash.jpg";
 import "./Home.css";
+import {Link} from "react-router-dom"
 
 export const Home = ({ employees, owners, pets }) => {
   return (
+    <Link to="/">
     <main>
       <section className="splash-image">
         <img src={splashImage} alt="Splash." />
@@ -12,6 +14,7 @@ export const Home = ({ employees, owners, pets }) => {
         <NewsletterForm />
       </section>
     </main>
+    </Link>
   );
 };
 
