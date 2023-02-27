de2889e0b38021133eb59c289b25735586240c71
joshuanelsondev <joshuanelsondev@gmail.com>
2023-02-27 17:07:23 -0500

    Complete Lab

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Footer from "./components/common/Footer";
 import Home from "./components/home/Home";
 import StaffList from "./components/staff/StaffList";
 import PetsList from "./components/pets/PetsList";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 /*
   Data
@@ -25,12 +26,17 @@ function App() {
 
   return (
     <div className="wrapper">
-      <Nav />
-      <Home employees={employees} owners={owners} pets={pets} />
-      <StaffList employees={employees} />
-      <PetsList pets={pets} />
-      <Footer />
+      <Router>
+        <Nav />
+          <Routes> 
+            <Route path="/" element={<Home employees={employees} owners={owners} pets={pets} />}/>
+            <Route path="/staff" element={<StaffList employees={employees} />} />
+            <Route path="/pets/*" element={<PetsList pets={pets} />} />
+          </Routes>
+        <Footer />
+      </Router>
     </div>
+    
   );
 }
 
