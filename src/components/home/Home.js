6d83e9bc8c6a5ecde49fe08963ada34f38f180d0
Kerridene Small <Kerridenesmall@gmail.com>
2023-02-27 17:36:44 -0500

    Completed React Router

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -2,8 +2,10 @@ import Highlights from "./Highlights";
 import NewsletterForm from "./NewsletterForm";
 import splashImage from "./andrew-s-unsplash.jpg";
 import "./Home.css";
+import { Link } from "react-router-dom";
 
 export const Home = ({ employees, owners, pets }) => {
+
   return (
     <main>
       <section className="splash-image">
@@ -11,8 +13,10 @@ export const Home = ({ employees, owners, pets }) => {
         <Highlights employees={employees} owners={owners} pets={pets} />
         <NewsletterForm />
       </section>
+
     </main>
   );
 };
+
 
 export default Home;
